0a9a61b9cb6d7ee4d01e12927a92c293d9086494
Ashley Upson <ashmisterupson@gmail.com>
2017-04-27 12:10:46 +0100

    Added to shop.

--- app/shop.js
+++ app/shop.js
@@ -3,3 +3,5 @@ var cpus = [16,	32,	64,	128, 256, 512, 1024], // Hertz; 1Hz = 1 byte of calculat
 	disks = [8, 16, 32, 64, 128, 256, 512], // KiB
 	NIC = [1, 2, 4, 8, 16, 32, 64], // KiB/s.
 	ISP = [2, 4, 8, 16, 32, 64, 128], // ISP network speeds.
+	memory = [4, 8, 16, 32, 64, 128, 256], // Ram levels.
+	software = [1, 2, 3, 4, 5, 6, 7]; // Maximum cores available for use.
